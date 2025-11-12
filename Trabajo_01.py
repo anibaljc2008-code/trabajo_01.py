61655a7a8c558e547ffc794aa2adf586088317e3
anibaljc2008-code <anibaljc2008@gmail.com>
2025-11-12 12:06:36 -0600

    Add files via upload

---
+++ Trabajo_01.py
@@ -0,0 +1,10 @@
+# Programa 1: cuadrado de un número
+
+# Leer un número
+numero = float(input("Ingrese un número: "))
+
+# Calcular el cuadrado
+cuadrado = numero ** 2
+
+# Mostrar el resultado
+print("El cuadrado de", numero, "es:", cuadrado)
